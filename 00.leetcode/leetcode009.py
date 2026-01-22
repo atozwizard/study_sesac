faea01e2edc271c1e58c0dfe8a358b97108ede86
이영기 <at.oz.wizard@gmail.com>
2026-01-22 09:19:43 +0900

    leetcode009 solve

--- 00.leetcode/leetcode009.py
+++ 00.leetcode/leetcode009.py
@@ -25,4 +25,33 @@ Constraints:
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        
+        #-(2**31) <= x <= (2**31 - 1)
+        # 주어진 정수의 절대값의 팰린드롬 확인 ture
+        # if 음수확인: return false
+        # s = str(abs(x))
+        # for i in range(len(s)) ?
+        # 투 포인터, 양 끝에서 하나씩 들어와?
+        # if [i] = [len(s)-i]: i += 1
+        #       if i = (len(s)-i) return true
+        # if [i] != [len(s)-i]: return false break
+        # 틀린것만 거르면 나머지 True
+        # i range //2
+
+        if (str(x))[0] == "-":
+            return False
+        strg_x= str(abs(x))
+        # for i in range(len(strg_x)//2):
+        #    """ # if strg_x[i] == strg_x[(len(strg_x)-1)-i] :
+        #     #     # i += 1
+        #     #     if i == len(strg_x)-i : #
+        #     #         return True
+        #     #     elif i == (len(strg_x) / 2) :
+        #     #         return True  """
+        #     if strg_x[i] != strg_x[(len(strg_x)-1)-i] :
+        #         return False
+            
+        # return True
+
+
+        # 거꾸로 뒤집은 문자열과 같으면 True 아니면 False
+        return strg_x == strg_x[::-1]
